9add3f861ed7e3edc357755381f73452f2667f24
ajay-chauhan-324 <ajubhai222@gmail.com>
2026-02-18 16:39:46 +0530

    update

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function albumitem() {
+function Albumitem({image,name,desc,id}) {
+
+  const navigate = useNavigate()
   return (
-    <div>
-      
+    <div onClick={()=>navigate('/album/${id}')} className='min-w-45 p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+      <img className='rounded' src={image} alt="" />
+      <p className='font-bold mt-2 mb-1'>{name}</p>
+      <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
   )
 }
 
-export default albumitem
+export default Albumitem
